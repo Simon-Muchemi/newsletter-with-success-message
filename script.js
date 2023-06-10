d31b3f3040513f805afca50130dce2f2fdc74063
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-10 13:55:01 +0300

    JS complete

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const form = document.getElementById('form')
 const emailInput = document.getElementById('email');
 const errorMsg = document.getElementById('error-msg');
 
-form.addEventListener('submit', function (){
+form.addEventListener('submit', function (event){
    let emailValue = emailInput.value;
     if (checkEmail(emailValue)){
         console.log('Should be true');
-       errorMsg.style.display = 'none';
+        errorMsg.style.display = 'none';
+        window.location.href = 'success-message.html';
    }else {
+        emailInput.style.color = "red";
+        emailInput.style.backgroundColor = "rgba(227, 122, 122, 0.61)";
         console.log("Should be false!");
-       errorMsg.style.display = 'inline';
+        errorMsg.style.display = 'inline';
+        event.preventDefault();
    }
 });
 function checkEmail(email){
