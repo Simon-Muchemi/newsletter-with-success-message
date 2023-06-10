d6084ce4e7c2196550921c42cb0234c963b3b25d
Simon-Muchemi <simonkenya846@gmail.com>
2023-06-10 21:20:19 +0300

    README file created

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const emailInput = document.getElementById('email');
 const errorMsg = document.getElementById('error-msg');
-
+const input = document.querySelector('input');
 validateEmail = function (event)
 {
     event.preventDefault();
@@ -13,6 +13,7 @@ validateEmail = function (event)
         emailInput.style.color = "red";
         emailInput.style.backgroundColor = "rgba(227, 122, 122, 0.61)";
         errorMsg.style.display = 'inline';
+        input.style.borderColor = 'red';
     }
 
 }
